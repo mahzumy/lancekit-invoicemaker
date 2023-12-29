456ec8800b9d2278f09bca6aacba96cf510d33d7
mahzumy <mahzumy34@gmail.com>
2023-12-30 06:52:29 +0700

    feat: pdtTheme

--- src/app/invoice-maker/page.tsx
+++ src/app/invoice-maker/page.tsx
@@ -1,10 +1,16 @@
-import { InvoiceGenerator } from '@/components/invoiceMaker/InvoiceGenerator'
-import Image from 'next/image'
+import { InvoiceGenerator } from "@/components/invoiceMaker/InvoiceGenerator";
+import Image from "next/image";
+import { HeaderAuth } from "@/components/header/header";
+import { getServerSession } from "next-auth";
+getServerSession;
+import { options } from "@/utils/nextauth/option";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(options);
   return (
     <div>
+      <HeaderAuth session={session} />
       <InvoiceGenerator />
     </div>
-  )
+  );
 }
