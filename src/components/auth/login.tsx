4ee756273f51639aa70d85c73e741dcbf28b0003
Nuha Fadh <fayn.deco@gmail.com>
2024-01-01 03:08:00 +0700

    background fixed

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -47,18 +47,16 @@ export const Login = () => {
       </div>
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="absolute inset-0 bg-zinc-900" style={{ backgroundImage: 'url("/asset/freelance-laptop.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}/>
           <div className="relative z-20 flex items-center text-lg font-medium">
             <img src="/asset/LanceKit Logo-dark.svg" width={120} alt="" />
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;It is essential to have good tools, but it is also essential that the tools should be used in the right way&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Wallace D. Wattles</footer>
             </blockquote>
               </div>
           </div>
